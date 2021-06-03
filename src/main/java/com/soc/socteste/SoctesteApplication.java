c99ee223397b375021e89a9b6bbbe0c9d274f8bf
aleAvian <ale.avian79@gmail.com>
2021-06-03 18:39:33 -0300

    initial Project

---
+++ src/main/java/com/soc/socteste/SoctesteApplication.java
@@ -0,0 +1,13 @@
+package com.soc.socteste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoctesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoctesteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/soc/socteste/config/JDBCConfig.java
@@ -0,0 +1,26 @@
+package com.soc.socteste.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+@Configuration
+@ComponentScan("com.soc.socteste")
+
+public class JDBCConfig {
+	@Bean
+	public DataSource mysqlDataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/socTeste");
+		dataSource.setUsername("root");
+		dataSource.setPassword("aleDB");
+
+		return dataSource;
+
+	}
+
+}
